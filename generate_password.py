8a0b52dd2c8da1903df9a2a22845e308304da745
Mikka-t <14maika18@gmail.com>
2023-10-28 10:37:38 +0900

    generate_password: delete seed

--- generate_password.py
+++ generate_password.py
@@ -2,9 +2,6 @@ import random
 import string
 
 def generate_password():
-    seed = "FLAG{Peek_into_the_past}"
-    random.seed(seed)
-    
     length = 12
 
     characters = string.ascii_letters + string.digits + string.punctuation
